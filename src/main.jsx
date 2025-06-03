29d791ca1d49ef37dedd58268dda611ed695b192
BGC <uthmanajanaku@gmail.com>
2025-06-03 15:40:44 +0100

    feat: added delete

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import AppProvider from "./context/AppContext.jsx";
 import Tenantprovider from "./context/TenantContext.jsx";
-
+import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
       <Tenantprovider>
         <App />
+        <ToastContainer position="top-center" />
       </Tenantprovider>
     </AppProvider>
   </StrictMode>
